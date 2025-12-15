3af591e2dd131df685376154f2c4a813499765a8
vinaybomma <vinaysb2000@gmail.com>
2025-12-15 23:51:05 +0530

    chore: expo router setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { SplashScreen } from "expo-router";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+
+export { default } from "expo-router";
+
+export function Root() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+  return null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
